0069fbdf56a2d2c960e4da21e434aa37c54bb011
Noel Joseph Jovero <njjovero26@gmail.com>
2025-08-27 18:21:48 -0400

    forgot to take out the second sun haha

--- CopyOfPicture.java
+++ CopyOfPicture.java
@@ -15,7 +15,6 @@ public class CopyOfPicture
     private Square window;
     private Triangle roof;
     private Circle sun;
-    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -27,7 +26,6 @@ public class CopyOfPicture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
-        sun2 = new Circle();
         drawn = false;
     }
 
@@ -58,12 +56,6 @@ public class CopyOfPicture
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
-            
-            sun2.changeColor("blue");
-            sun2.moveHorizontal(30);
-            sun2.moveVertical(-90);
-            sun2.changeSize(80);
-            sun2.makeVisible();
             drawn = true;
         }
     }
